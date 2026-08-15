8ed3ddd995e6e43463865d307fbf8938c74e110f
bmxer32 <krotovnot@gmail.com>
2026-08-15 17:30:17 +0300

    Вход через Google в браузере

--- play/flutter_bootstrap.js
+++ play/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2287585656" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2280320400" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
