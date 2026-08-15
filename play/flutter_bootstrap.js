1d8c480fe3537032a2b0078a772a7be9e8aa9ae5
bmxer32 <krotovnot@gmail.com>
2026-08-15 17:12:45 +0300

    Иконки: пересобраны из 512, фавикон отдельной композицией
    192-е были обрезком: headless-браузер при маленьком окне отрисовал
    не то, а проверил я тогда только 512-е. Теперь уменьшены из них
    ресемплингом.
    
    Фавикон нарисован плотнее иконки приложения — во вкладке шестнадцать
    точек, и поля съедали на них весь логотип. Рядом .ico со всеми
    ходовыми размерами.

--- play/flutter_bootstrap.js
+++ play/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1178762811" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "354304368" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
